c412f035b52ee265dff3f52a5bd66474e0a34715
Tilman Kuehner <kuehner@safead.de>
2021-04-01 13:57:44 +0200

    get current camera image

--- cvat-ui/src/components/annotation-page/standard-workspace/camera-images.tsx
+++ cvat-ui/src/components/annotation-page/standard-workspace/camera-images.tsx
@@ -4,18 +4,42 @@ import { Row, Col, Divider } from 'antd';
 // import SVG from 'svg.js';
 import SVG from 'svg.js';
 import 'cvat-canvas/src/typescript/svg.patch';
+import { Image as CanvasImage } from 'cvat-canvas/src/typescript/canvasModel';
 import './canvas-container.css';
 // import Snap from 'snapsvg';
+import { CombinedState } from 'reducers/interfaces';
+import { connect } from 'react-redux';
 
-class LeftCameraImages extends React.PureComponent {
+interface Props {
+    activeLabelID: number;
+    frameData: any;
+}
+
+function mapStateToProps(state: CombinedState): Props {
+    const {
+        annotation: {
+            canvas: { activeControl, instance: canvasInstance },
+            drawing: { activeLabelID, activeObjectType },
+            player: {
+                frame: { data: frameData, number: frame, fetching: frameFetching },
+                frameAngles,
+            },
+        },
+    } = state;
+    return {
+        activeLabelID,
+        frameData,
+    };
+}
+
+class LeftCameraImages extends React.PureComponent<Props> {
     canvas: HTMLDivElement;
     background: HTMLCanvasElement;
     overlay: HTMLCanvasElement;
-    image: HTMLImageElement;
     content: SVGSVGElement;
-    adoptedContent: SVG.Container;
+    image: CanvasImage | null;
 
-    constructor(props: any) {
+    constructor(props: Props) {
         super(props);
         this.canvas = window.document.createElement('div');
         this.background = window.document.createElement('canvas');
@@ -25,51 +49,69 @@ class LeftCameraImages extends React.PureComponent {
         this.overlay = window.document.createElement('canvas');
         this.canvas.appendChild(this.overlay);
 
-        // this.content.setAttribute("width", String(this.background.width))
-        // this.content.setAttribute("height", String(this.background.height))
-        // this.content.setAttribute("position", "absolute")
-        // this.content.setAttribute("top", '0px')
-        // this.content.setAttribute("left", '0px');
-
-        // this.adoptedContent = SVG.adopt((this.content as any) as HTMLElement) as SVG.Container;
-        // this.canvas.appendChild(this.content);
-
-        // this.adoptedContent.rect().size(100,100).move(0,0);
+        this.image = null;
 
         this.background.width = 300;
-        this.background.height = 300;
         this.overlay.width = 300;
-        this.overlay.height = 300;
 
-        let ctx = this.background.getContext('2d');
-        let image = new Image();
-        image.onload = () => {
-            // Make sure the image is loaded first otherwise nothing will draw.
-            var ratio = image.height / image.width;
-            console.log('ratio', ratio, this.background.width, this.background.width);
-            ctx.drawImage(
-                image,
-                0,
-                0,
-                image.width,
-                image.height, // source rectangle
-                0,
-                0,
-                this.background.width,
-                ratio * this.background.width,
-            );
-        };
-        image.src = 'https://assets.pixolum.com/blog/wp-content/uploads/2019/09/Blumen-Fotografieren-50mm-800x533.jpg';
+        // let image = new Image();
+        // image.onload = () => {
+        //     // Make sure the image is loaded first otherwise nothing will draw.
+        //     var ratio = image.height / image.width;
+        //     let targetHeight = ratio * this.background.width;
+        //     this.background.height = targetHeight;
+        //     this.overlay.height = targetHeight;
+        //     ctx.drawImage(
+        //         image,
+        //         0,
+        //         0,
+        //         image.width,
+        //         image.height, // source rectangle
+        //         0,
+        //         0,
+        //         this.background.width,
+        //         targetHeight,
+        //     );
+        // };
+        // image.src = 'https://assets.pixolum.com/blog/wp-content/uploads/2019/09/Blumen-Fotografieren-50mm-800x533.jpg';
     }
 
     public componentDidMount(): void {
-        // see https://eloquentjavascript.net/17_canvas.html
-        let cx = this.overlay.getContext('2d');
-        cx.strokeStyle = 'red';
-        cx.strokeRect(100, 100, 100, 100);
-
         const [wrapper] = window.document.getElementsByClassName('cvat-side-image-container');
         wrapper.appendChild(this.html());
+
+        this.props.frameData
+            .data((): void => {
+                this.image = null;
+            })
+            .then((data: CanvasImage): void => {
+                console.log('got image');
+                let image = data.imageData;
+                let ctx = this.background.getContext('2d');
+                var ratio = image.height / image.width;
+                let targetHeight = ratio * this.background.width;
+                this.background.height = targetHeight;
+                this.overlay.height = targetHeight;
+                ctx.drawImage(
+                    image,
+                    0,
+                    0,
+                    image.width,
+                    image.height, // source rectangle
+                    0,
+                    0,
+                    this.background.width,
+                    targetHeight,
+                );
+                // see https://eloquentjavascript.net/17_canvas.html
+                let cx = this.overlay.getContext('2d');
+                cx.strokeStyle = 'red';
+                cx.lineWidth = 3;
+                cx.strokeRect(20, 20, 50, 50);
+            })
+            .catch((exception: any): void => {
+                throw exception;
+            });
 
         // var draw = SVG('mydiv').size(300, 130)
         // var rect = draw.rect(100, 100).fill('#f06').move(20, 20)
@@ -105,4 +147,4 @@ class LeftCameraImages extends React.PureComponent {
     }
 }
 
-export default LeftCameraImages;
+export default connect(mapStateToProps)(LeftCameraImages);
